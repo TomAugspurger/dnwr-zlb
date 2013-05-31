103859ab326f7fb2cce2b1318cdd6a3f7017135d
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-05-31 09:39:26 -0500

    Some sanity checkerss and a writer.

--- data_wrangling/cps_wrangling/panel_construction/generic_data_dictionary_parser.py
+++ data_wrangling/cps_wrangling/panel_construction/generic_data_dictionary_parser.py
@@ -180,7 +180,7 @@ class Parser(object):
 
     def __init__(self, infile, outfile, regex=None):
         self.infile = infile
-        self.outfile = outfile
+        self.outfile = outfile  # Full path for store.
         self.dataframes = []
         self.previous_line = None
         self.next_line = None
@@ -191,6 +191,7 @@ class Parser(object):
         self.pos_len = 1
         self.pos_start = 2
         self.pos_end = 3
+        self.store_name = self.get_store_name()  # name in Store.
 
     def run(self):
         with open(self.infile, 'r') as f:
@@ -225,6 +226,48 @@ class Parser(object):
             else:
                 self.holder.append(formatted)
 
+    def get_store_name(self):
+        """
+        #-----------------------------------------------------------------------
+        NOTE: Usually, the documentation from January applies to an entire
+        year. Execptions are 1984-1985 and 1994-1995. The January 1984
+        documentation is used through to June 1985. The July 1985 documentation
+        applies to the remainder of 1985. For 1994-1995, the January 1994
+        documentation is used through August 1995. The September 1995
+        documentation serves for the rest of the year.
+
+        #-----------------------------------------------------------------------
+        Not sure about jan2003 going till april 2004.
+
+
+        We have {
+                jan1989: [1989-01,1991-12], jan1992: [1992-01,1993-12],
+                jan1994: [1994-01,1995-08], sep1995: [1995-09,1997-12],
+                jan1998: [1998-01,2002-12], jan2003: [2003-01,2004-04],
+                may2004: [2004-05,2005-07], aug2005: [2005-08,2006-12],
+                jan2007: [2007-01,2008-12], jan2009: [2009-01,2009-12],
+                jan2010: [2010-01,2012-04], may2012: [2012-05,2012-12]
+                jan2013: [2013-01,2013-03]
+                }
+        """
+        dict_ = {'cps89.ddf': 'jan1989',
+                 'cps92.ddf': 'jan1992',
+                 # MISSING 1994
+                 'cpsbsep95.ddf': 'sep1995',
+                 'cpsbjan98.ddf': 'jan1998',  # renamed this infile
+                 'cpsbjan03.ddf': 'jan2003',
+                 'cpsbmay04.ddf': 'may2004',
+                 'cpsbaug05.ddf': 'aug2005',
+                 'cpsbjan07.ddf': 'jan2007',
+                 'cpsbjan09.ddf': 'jan2009',
+                 'cpsbjan10.ddf': 'jan2010',
+                 'cpsbmay12.ddf': 'may2012',
+                 # Missing 2013. use alt parser
+                 }
+        inname = self.infile.split('/')[-1]
+        store_name = dict_[inname]
+        return store_name
+
     def handle_padding(self, formatted, f):
         """
         CPS left out some padding characters.
@@ -239,7 +282,7 @@ class Parser(object):
 
         # next_formatted = self.formatter(maybe_groups)
         last_formatted = self.holder[-1]
-        pad_len = formatted[self.pos_start] - last_formatted[self.pos_end]
+        pad_len = formatted[self.pos_start] - last_formatted[self.pos_end] - 1
         pad_str = last_formatted[self.pos_end] + 1
         pad_end = pad_len + pad_str - 1
         pad = ('PADDING', pad_len, pad_str, pad_end)
@@ -257,10 +300,10 @@ class Parser(object):
 
         if len(self.dataframes) == 0:
             self.dataframes.append(df)
-            common_index_pt = df[df['start'] == formatted[self.pos_end]].index[0]
+            common_index_pt = df[df['start'] == formatted[self.pos_end]].index[0] - 1
             self.common = df.ix[:common_index_pt]
         else:
-            df = pd.concat([self.common, df])
+            df = pd.concat([self.common, df], ignore_index=True)
             self.dataframes.append(df)
 
         self.holder = [formatted]  # next line
@@ -280,16 +323,32 @@ class Parser(object):
         print(id_)
         return (id_, length, start, end)
 
+    def len_checker(self):
+        # Will fail cause CPS screwed up w/ padding.
+        for df in self.dataframes:
+            assert (df.end - df.start == df.length - 1).all()
+
+    def con_checker(self):
+        for df in self.dataframes:
+            assert (df.end.shift() - df.start + 1 == 0)[1:].all()
+
+    def writer(self):
+        """
+        Once you have all the dataframes, write them to that outfile,
+        an HDFStore.
+        """
+        store = pd.HDFStore(self.outfile)
+        for df in self.dataframes:
+            try:
+                store.remove('monthly/dd/' + self.store_name)
+            except KeyError:
+                pass
+            store.append('monthly/dd/' + self.store_name, df)
+        store.close()
+
 if __name__ == '__main__':
     """
     Third arg must be True if you want to match a march dd.
-    
-
-    Getting messy. When you return do this.
-    make a class. let the class know the position, lenght, etc.
-    of the previous line.  If the diff is negative add padding,
-    if the diff is positive, make a new push previous lines into
-    a dataframe and fill in the new one.
     """
 
     import sys
@@ -309,23 +368,7 @@ if __name__ == '__main__':
             except IOError:
                 print('No File for {}'.format(infile))
     #--------------------------------------------------------------------------
-    # Monthly regex [H,HG,L,A,C,M][$-%]|PADDING
-    # e.g.
-    # May be padding from [31 - 37]
     else:
-        reg = make_reg()
-        main(infile)
+        kls = Parser(infile)
+        kls.run()
         print('Added {}'.format(infile))
-
-def run():
-    infile = '/Volumes/HDD/Users/tom/DataStorage/CPS/monthly/cps89.ddf'
-    f = open('/Volumes/HDD/Users/tom/DataStorage/CPS/monthly/cps89.ddf')
-    ids, lengths, starts, ends = [], [], [], []
-    list_group = (ids, lengths, starts, ends)
-    regex = make_reg()
-    year = re.match(r'\w*(\d{2})', infile.split('/')[-1]).groups()[0]
-    for line in f:
-        grouped = month_item(line, reg=regex)
-        if grouped:  # There is a match
-            month_dict_constructor(line, grouped, list_group)
-    return ids, lengths, starts, ends
