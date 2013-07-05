9247015618bb0207e0189b3fa22192681c89a59c
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-07-05 16:02:48 -0500

    Reorganization. No new code.

--- data_wrangling/cps_wrangling/panel_construction/make_hdf_store.py
+++ data_wrangling/cps_wrangling/panel_construction/make_hdf_store.py
@@ -40,75 +40,11 @@ import subprocess
 import pathlib
 import pandas as pd
 from matplotlib.cbook import flatten
+
+
 #-----------------------------------------------------------------------------
-# Helper Functions
-
-
-def writer(df, name, store_path, settings):
-    """
-    Write the dataframe to the HDFStore. Non-pure.
-
-    Parameters
-    ----------
-    df: DataFrame to be writter
-    name: name in the table; will be prepended with '/monhtly/data/m'
-    store_path: path to the store.  Get from settings.
-
-    Returns
-    -------
-    None - IO
-    """
-    with pd.get_store(store_path) as store:
-        try:
-            store.remove('/monthly/data/' + name)
-        except KeyError:
-            pass
-        store.append('/monthly/data/' + name, df)
-
-    with open(settings["store_log"], 'a') as f:
-        f.write('PASSED {}\n'.format(name))
-
-
-def dedup_cols(df):
-    """
-    Drops columns that are duplicated.  Have to transpose for unknown reason.
-    I'm hitting multiple PADDING's, that should be it.
-
-    Parameters
-    ----------
-    df : DataFrame
-
-    Returns
-    df : Same DataFrame, less the dupes.
-    """
-    idx = df.columns
-    dupes = idx.get_duplicates()
-    print("Duplicates: {}".format(dupes))
-    return df.T.drop(dupes).T
-
-
-def pre_process(df, ids):
-    """
-    Get DataFrame ready for writing to store.
-
-    Makes (hopefully) unique index and makes types numeric.
-
-    Parameters
-    ----------
-    df  : DataFrame
-    ids : Columns to be used as the index.
-
-    Returns
-    -------
-    df : DataFrame
-    """
-    df = dedup_cols(df)
-    # May want to be careful with index here.
-    # forcing numeric chops of leading zeros.
-    df = df.convert_objects(convert_numeric=True)
-    df = df.set_index(ids)
-    return df
-
+# File Handling / IO
+#-----------------------------------------------------------------------------
 
 class FileHandler(object):
     """
@@ -167,68 +103,75 @@ class FileHandler(object):
             os.remove(self.new_path)
 
 
-def get_dd(fname, settings=None):
+def writer(df, name, store_path, settings):
     """
-    Helper to get the data dictionary associated with a given month's filename.
+    Write the dataframe to the HDFStore. Non-pure.
 
     Parameters
     ----------
-    fname: str, either full or shortened should work.
-    settings: str or None.  Path to the settings file
+    df: DataFrame to be writter
+    name: name in the table; will be prepended with '/monhtly/data/m'
+    store_path: path to the store.  Get from settings.
 
     Returns
     -------
-    dd: str, name of data dictionary.
+    None - IO
     """
-    if settings is None:
-        settings = json.load(open('settings.txt'))
-    just_name = fname.split('/')[-1].split('.')[0]
-    return settings['month_to_dd_by_filename'][just_name]
+    with pd.get_store(store_path) as store:
+        try:
+            store.remove('/monthly/data/' + name)
+        except KeyError:
+            pass
+        store.append('/monthly/data/' + name, df)
+
+    with open(settings["store_log"], 'a') as f:
+        f.write('PASSED {}\n'.format(name))
 
 
-def get_id(target, store):
-    """
-    Target is a str, e.g. HHID; This finds all this little
-    idiosyncracies.
-    """
-    for key in store.keys():
-        dd = store.select(key)
-        yield key, dd.id[dd.id.str.contains(target)]
+#-----------------------------------------------------------------------------
+# Duplicate Handling
+#-----------------------------------------------------------------------------
 
 
-def find_attr(attr, fields=None, dd_path=None, settings=None):
+def dedup_cols(df):
     """
-    Dictionary may lie.  Check here.
+    Drops columns that are duplicated.  Have to transpose for unknown reason.
+    I'm hitting multiple PADDING's, that should be it.
 
     Parameters
     ----------
-    attr: str; e.g. "AGE", "RACE", "SEX"
-    df: Index; probably columns of the dataframe.
-    dd_path : str; path inside the store.
-    settings: dict with "store_path"
+    df : DataFrame
+
+    Returns
+    df : Same DataFrame, less the dupes.
+    """
+    idx = df.columns
+    dupes = idx.get_duplicates()
+    print("Duplicates: {}".format(dupes))
+    return df.T.drop(dupes).T
+
+
+def pre_process(df, ids):
+    """
+    Get DataFrame ready for writing to store.
+
+    Makes (hopefully) unique index and makes types numeric.
+
+    Parameters
+    ----------
+    df  : DataFrame
+    ids : Columns to be used as the index.
+
     Returns
     -------
-
-    List of strs possible matches.
+    df : DataFrame
     """
-    if settings is None:
-        settings = settings = json.load(open('settings.txt'))
-
-    store = pd.HDFStore(settings["store_path"])
-
-    if fields and dd_path:
-        raise ValueError('One of fields and dd_path must be specified.')
-    elif fields is None and dd_path is None:
-        raise ValueError('One of fields and dd_path must be specified.')
-    elif dd_path:
-        dd = store.select(dd_path)
-        fields = dd.id.tolist()
-
-    store.close()
-
-    match_with = re.compile(r'[\w|$%\-]*' + attr + r'[\w|$%\-]*')
-    maybe_matches = (match_with.match(x) for x in fields)
-    return [x.string for x in filter(None, maybe_matches)]
+    df = dedup_cols(df)
+    # May want to be careful with index here.
+    # forcing numeric chops of leading zeros.
+    df = df.convert_objects(convert_numeric=True)
+    df = df.set_index(ids)
+    return df
 
 
 def drop_invalid_indicies(df, dd_name=None):
@@ -255,56 +198,6 @@ def make_regex(style=None):
         return re.compile(r'(\w+)[\s\t]*(\d{1,2})[\s\t]*(.*?)[\s\t]*\(*(\d+)\s*-\s*(\d+)\)*$')
     elif style is 'jan1998':
         return re.compile(r'D (\w+) \s* (\d{1,2}) \s* (\d*)')
-
-
-def get_definition(code, dd_path=None, style=None):
-    """
-    Get the definition for a code.
-
-    Maybe add option to pass dd_name with a lookup from settings as convinence.
-    """
-
-    regex = make_regex(style)
-
-    def dropper(line):
-        maybe_match = regex.match(line)
-        try:
-            line_code = maybe_match.groups()[0]
-            return line_code
-        except AttributeError:
-            return None
-
-    def get_def(dd):
-        """
-        Next of dd is the line you start with.  Now consume up to next match.
-        """
-        top_line = [''.join(list(next(dd)))]
-        rest = it.takewhile(lambda x: regex.match(x) is None, dd)
-        rest = [''.join(x) for x in rest]
-        top_line.append(rest)
-        return top_line
-
-    with open(dd_path) as dd:
-        gen = it.dropwhile(lambda x: dropper(x) != code, dd)
-        definition = get_def(gen)
-        return definition
-
-
-def log_and_store(df):
-    with open('ready_to_store.txt', 'w') as f:
-        if df.index.is_unique:
-            f.write('Ready: {}\n'.format(df.index.name))
-        else:
-            dupes = df.index.get_duplicates()
-            f.write('Dupes: {}\n'.format(df.index.name))
-            f.write("\t\t {}".format(dupes))
-
-
-def get_skips(file_):
-    with open(file_, 'r') as f:
-        skips = [line.split(' ')[-1].rstrip()
-                 for line in f if line.startswith('PASSED')]
-    return skips
 
 
 def handle_dupes(df, settings):
@@ -352,7 +245,132 @@ def handle_89_pt2(df):
     df = df[df['H____RECTYP'] == 1]
     return df
 
+#-----------------------------------------------------------------------------
+# Helpers
+#-----------------------------------------------------------------------------
 
+
+def get_dd(fname, settings=None):
+    """
+    Helper to get the data dictionary associated with a given month's filename.
+
+    Parameters
+    ----------
+    fname: str, either full or shortened should work.
+    settings: str or None.  Path to the settings file
+
+    Returns
+    -------
+    dd: str, name of data dictionary.
+    """
+    if settings is None:
+        settings = json.load(open('settings.txt'))
+    just_name = fname.split('/')[-1].split('.')[0]
+    return settings['month_to_dd_by_filename'][just_name]
+
+
+def get_id(target, store):
+    """
+    Target is a str, e.g. HHID; This finds all this little
+    idiosyncracies.
+    """
+    for key in store.keys():
+        dd = store.select(key)
+        yield key, dd.id[dd.id.str.contains(target)]
+
+
+def get_definition(code, dd_path=None, style=None):
+    """
+    Get the definition for a code.
+
+    Maybe add option to pass dd_name with a lookup from settings as convinence.
+    """
+
+    regex = make_regex(style)
+
+    def dropper(line):
+        maybe_match = regex.match(line)
+        try:
+            line_code = maybe_match.groups()[0]
+            return line_code
+        except AttributeError:
+            return None
+
+    def get_def(dd):
+        """
+        Next of dd is the line you start with.  Now consume up to next match.
+        """
+        top_line = [''.join(list(next(dd)))]
+        rest = it.takewhile(lambda x: regex.match(x) is None, dd)
+        rest = [''.join(x) for x in rest]
+        top_line.append(rest)
+        return top_line
+
+    with open(dd_path) as dd:
+        gen = it.dropwhile(lambda x: dropper(x) != code, dd)
+        definition = get_def(gen)
+        return definition
+
+
+def find_attr(attr, fields=None, dd_path=None, settings=None):
+    """
+    Dictionary may lie.  Check here.
+
+    Parameters
+    ----------
+    attr: str; e.g. "AGE", "RACE", "SEX"
+    df: Index; probably columns of the dataframe.
+    dd_path : str; path inside the store.
+    settings: dict with "store_path"
+    Returns
+    -------
+
+    List of strs possible matches.
+    """
+    if settings is None:
+        settings = settings = json.load(open('settings.txt'))
+
+    store = pd.HDFStore(settings["store_path"])
+
+    if fields and dd_path:
+        raise ValueError('One of fields and dd_path must be specified.')
+    elif fields is None and dd_path is None:
+        raise ValueError('One of fields and dd_path must be specified.')
+    elif dd_path:
+        dd = store.select(dd_path)
+        fields = dd.id.tolist()
+
+    store.close()
+
+    match_with = re.compile(r'[\w|$%\-]*' + attr + r'[\w|$%\-]*')
+    maybe_matches = (match_with.match(x) for x in fields)
+    return [x.string for x in filter(None, maybe_matches)]
+
+#-----------------------------------------------------------------------------
+# Logging
+#-----------------------------------------------------------------------------
+
+
+def log_and_store(df):
+    with open('ready_to_store.txt', 'w') as f:
+        if df.index.is_unique:
+            f.write('Ready: {}\n'.format(df.index.name))
+        else:
+            dupes = df.index.get_duplicates()
+            f.write('Dupes: {}\n'.format(df.index.name))
+            f.write("\t\t {}".format(dupes))
+
+
+def get_skips(file_):
+    with open(file_, 'r') as f:
+        skips = [line.split(' ')[-1].rstrip()
+                 for line in f if line.startswith('PASSED')]
+    return skips
+
+
+#-----------------------------------------------------------------------------
+# MISC
+#-----------------------------------------------------------------------------
 def get_subset(df, settings, dd_name, quiet=True):
     """
     Select only those columns specified under settings.
