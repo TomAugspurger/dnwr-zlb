db7dbe3b862643d7e662fe50d37ff88f58e30a42
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-07-19 10:46:48 -0500

    CLN: cleanup
    more cleanup.

--- model/vf_iteration.py
+++ model/vf_iteration.py
@@ -3,11 +3,18 @@ from __future__ import division
 import json
 
 import numpy as np
-from scipy.stats import lognorm
+from scipy import stats
+from scipy.stats import norm, lognorm
 #-----------------------------------------------------------------------------
-with open('parameters.json') as f:
-    params = json.load(f)
 
+
+def load_params(pth='parameters.json'):
+    with open(pth) as f:
+        params = json.load(f)
+
+    return params
+
+params = load_params()
 VALUE, DESCRIPTION = 0, 1
 
 wl = params['wl'][VALUE]     # wage lower bound
@@ -18,15 +25,20 @@ w_grid = np.linspace(wl, wu, wn)
 pi_grid = np.linspace(0, 2.5, 10)
 pibar = 2.0  # ss inflation
 
+
 #-----------------------------------------------------------------------------
 # Setup the Distribution
 # If log x is normally distributed with mean mu and variance sigma**,
 # then x is log-normally distributed with shape paramter sigma and
 # scale parameter exp(mu).
-sigma = params['sigma']
-mu = - (sigma ** 2) / 2
 
-z_dist = norm(loc=mu, scale=sigma)  # z_dist ~ lognormally. E[exp(z)] = 1
+
+def setup_shocks():
+    sigma = params['sigma'][VALUE]
+    mu = - (sigma ** 2) / 2
+
+    z_dist = norm(loc=mu, scale=sigma)  # z_dist ~ lognormally. E[exp(z)] = 1
+    pass
 
 
 def truncate_normal(original, lower, upper):
@@ -79,6 +91,7 @@ def ut_l(wage, shock, agg_L, params):
     eta = params['eta'][0]
     gamma = params['gamma'][0]
     return (wage ** (1 - eta) - (gamma / (gamma + 1)) * shock * (wage ** (-eta) * agg_L) ** (gamma / gamma + 1))
+
 
 def iter_vf(params):
     VALUE, DESCRIPTION = 0, 1
