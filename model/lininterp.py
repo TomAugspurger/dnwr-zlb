6cd35ebb8c115abd70301ffd1aee1749d1e72dc4
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-07-29 12:33:10 -0500

    ENH: Added piecewise cubic interpolation.
    Probably time to ditch this module.

--- model/lininterp.py
+++ model/lininterp.py
@@ -4,6 +4,7 @@
 # Corresponds to: Listing 6.4
 
 from scipy import interp
+from scipy.interpolate import pchip_interpolate
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -11,19 +12,31 @@ import matplotlib.pyplot as plt
 class LinInterp(object):
     "Provides linear interpolation in one dimension."
 
-    def __init__(self, X, Y):
+    def __init__(self, X, Y, kind='linear'):
         """Parameters: X and Y are sequences or arrays
         containing the (x,y) interpolation points.
+
+        kind : type of interpolation. one of {"linear", "pchip"}
         """
         self.X = np.sort(X)
         self.Y = Y
+
+        if kind == 'linear':
+            self.interp = interp
+        elif kind == 'pchip':
+            self.interp = pchip_interpolate
+        else:
+            raise NotImplementedError
 
     def __call__(self, z):
         """Parameters: z is a number, sequence or array.
         This method makes an instance f of LinInterp callable,
         so f(z) returns the interpolation value(s) at z.
         """
-        return interp(z, self.X, self.Y)
+        try:
+            return self.interp(z, self.X, self.Y)
+        except ValueError:
+            return self.interp(self.X, self.Y, z)
 
     def __add__(self, other):
         assert (self.X == other.X).all()
@@ -38,3 +51,7 @@ class LinInterp(object):
 
     def inverse(self):
         return LinInterp(self.Y, self.X)
+
+    def __mul__(self, other):
+        """Elementwise Multiplication"""
+        return LinInterp(X, Y * self.Y)
