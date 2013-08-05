e6f45fe4cfce0c373d364b41c85688546f099257
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-08-05 12:10:16 -0500

    ENH: Added more logging/writing and reading for analyze run.

---
+++ model/analyze_run.py
@@ -0,0 +1,62 @@
+from collections import defaultdict
+import pickle
+
+from pathlib import Path
+
+from helpers import load_params
+
+
+def bin_results(res_dir):
+    """
+    Take a Path object and return a dict based on type of result.
+
+    Parameters
+    ----------
+
+    res_dir: pathlib.Path iterable object
+
+    Returns
+    -------
+
+    binned_results : dict of type -> filename
+    """
+    binned_results = defaultdict(list)
+    groups = ['results', 'vf', 'ws', 'rigid_output']
+    for child in res_dir:
+        if child.match('.DS_Store'):
+            continue
+        for group in groups:
+            if child.name.startswith(group):
+                binned_results[group].append(str(child))
+    return binned_results
+
+
+def load_pickleable(pth):
+    """
+    Use in a comprehension for ws, vf.
+        pth is a str.
+    """
+    with open(pth, 'r') as f:
+        res = (pickle.load(f))
+    return res
+
+
+def load_single(pth):
+    """
+    Use for things like rigid_output, i.e. just single lines.
+    """
+    with open('pth', 'r') as f:
+        res = float(f.read())
+    return res
+
+
+if __name__ == '__main__':
+    params = load_params()
+    res_path = params['results_path'][0]
+    try:
+        res_dir = Path(res_path)
+    except TypeError:
+        res_dir = Path(str(res_path))
+
+    binned = bin_results(res_dir)
+    print(binned)
--- model/run_value_function.py
+++ model/run_value_function.py
@@ -2,12 +2,12 @@ from __future__ import division
 
 import pickle
 
-from helpers import load_params
+from helpers import load_params, ss_wage_flexible
 from joblib import Parallel, delayed
 import numpy as np
 
 from gen_interp import Interp
-from value_function import iter_bellman
+from value_function import get_rigid_output, g_p, iter_bellman
 
 
 def iter_bellman_wrapper(pi):
@@ -30,11 +30,28 @@ def iter_bellman_wrapper(pi):
     Tv, ws, rest = iter_bellman(
         v, tol=0.005, strict=False, log=False, params=params, pi=pi)
     res_dict = {'Tv': Tv, 'ws': ws, 'rest': rest}
-    write_results(res_dict, pi)
+
     # full_df = pd.DataFrame({'vfs': vfs, 'wss': wss,
                             # 'rests': rests, 'wss': wss})
     # full_df.to_csv('results/vf_results' + str(pi) + '.csv')
+
+    #--------------------------------------------------------------------------
+    gp, flex_ws = get_wage_distribution(params)
+    res_dict['gp'] = gp
+    rigid_out = get_rigid_output(params, ws, flex_ws, gp)
+    res_dict['rigid_out'] = rigid_out
+    write_results(res_dict, pi)
     pass
+
+
+def get_wage_distribution(params):
+    shock = params['shock'][0]
+    grid = params['grid'][0]
+    # fine_grid = params['fine_grid'][0]
+    g0 = Interp(grid, grid/4, kind='pchip')
+    gp = g_p(g0, params['ln_dist'][0], params)
+    flex_ws = Interp(shock, ss_wage_flexible(params, shock=shock))
+    return gp, flex_ws
 
 
 def write_results(res_dict, pi):
@@ -49,6 +66,10 @@ def write_results(res_dict, pi):
         pickle.dump(res_dict['Tv'], f)
     with open('results/ws_' + piname + '.pkl', 'w') as f:
         pickle.dump(res_dict['ws'], f)
+    with open('results/gp_' + piname + '.pkl', 'w') as f:
+        pickle.dump(res_dict['gp'], f)
+    with open('results/rigid_output_' + piname + '_.txt', 'w') as f:
+        f.write(str(res_dict['rigid_out']))
     res_dict['rest'].to_hdf('results/results_' + piname + '.h5',
                             'pi_' + piname)
     print('Added results for {}'.format(pi))
