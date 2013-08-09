67d497882ad370e7ebfe005442f5b8ffd58b000a
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-08-09 10:48:09 -0500

    testing a setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='dnwr-zlb',
+      version='0.1.0',
+      description='Second Year Paper',
+      author='Tom Augspurger',
+      author_email='thomas-augspurger@uiowa.edu',
+      packages=['pandas'],
+      )
