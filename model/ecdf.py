fa6ae69c79ecce8379902755f2a5d4c5e513e36e
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-08-21 08:27:09 -0500

    ENH: Add range method to ecdf

---
+++ model/ecdf.py
@@ -0,0 +1,51 @@
+"""
+Origin: QEwP by John Stachurski and Thomas J. Sargent
+Date:   5/2013
+File:   ecdf.py
+
+Implements the empirical distribution function.
+
+"""
+from __future__ import division
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class ecdf:
+
+    def __init__(self, observations):
+        if not is_mono(observations):
+            observations = np.sort(observations)
+        self.observations = np.asarray(observations).reshape(-1, 1)
+        self.n = len(self.observations)
+
+    def __call__(self, x):
+        """Handles vectors for x"""
+        return (self.observations <= x).sum(0) / self.n
+
+    def plot(self, ax=None, a=None, b=None, **kwargs):
+
+        # === choose reasonable interval if [a, b] not specified === #
+        if not a:
+            a = self.observations[0] - self.observations.std()
+        if not b:
+            b = self.observations[-1] + self.observations.std()
+
+        # === generate plot === #
+        x_vals = np.linspace(a, b, num=self.n)
+        if ax is None:
+            fig, ax = plt.subplots()
+        ax.plot(x_vals, self(x_vals.T), **kwargs)
+        plt.show()
+        return ax
+
+    def get_range(self):
+        return self.observations.min(), self.observations.max()
+
+
+def is_mono(x):
+    if (np.diff(x) >= 0).all():
+        return True
+    else:
+        return False
