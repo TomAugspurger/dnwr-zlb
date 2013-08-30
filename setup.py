5596ba918ab90cd75b0c6cf21603d9b6d56a8525
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-08-30 16:22:17 -0500

    BLD:  Staring a setup.py process.  Not ready for master.

--- setup.py
+++ setup.py
@@ -1,9 +1,29 @@
 from distutils.core import setup
 
+import os
+import sys
+import shutil
+import warnings
+
+# may need to work around setuptools bug by providing a fake Pyrex
+try:
+    import Cython
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), "fake_pyrex"))
+except ImportError:
+    pass
+
+
 setup(name='dnwr-zlb',
       version='0.1.0',
-      description='Second Year Paper',
+      description='Reproduction file for my second year paper.',
       author='Tom Augspurger',
       author_email='thomas-augspurger@uiowa.edu',
-      packages=['pandas'],
+      packages=['model',
+                'data_wrangling',
+                'data_wrangling.cps_wrangling',
+                'data_wrangling.cps_wrangling.panel_construction',
+                'data_wrangling.greenbook_wrangling',
+                'data_wrangling.nipa_wrangling',
+                'writing',
+                'vb_suit'],
       )
