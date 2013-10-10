b2ee4ee777ad4b72623dfa987895da30c79befd0
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-10-10 16:08:08 -0500

    Don't overwrite downloaded files.

--- data_wrangling/cps_wrangling/panel_construction/monthly_data_downloader.py
+++ data_wrangling/cps_wrangling/panel_construction/monthly_data_downloader.py
@@ -11,7 +11,13 @@ Change the variable out_dir as necessary for your machine's filesystem.
 
 http://www.nber.org/data/cps_basic.html
 """
+# TODO: Get list of files from from settings. Currently just grabs all new.
+
+from functools import partial
+from itertools import ifilter
 import re
+import os
+from os.path import exists
 import urllib2
 
 from lxml.html import parse
@@ -36,13 +42,24 @@ def downloader(link, out_dir, dl_base="http://www.nber.org/cps-basic/"):
         f.write(content.read())
 
 
+def _exists(path_name):
+    no_ext = path_name.split('.')[0]
+    if exists(path_name) or exists(no_ext + '.gz') or exists(no_ext + '.gz'):
+        return True
+
+
 def main(out_dir, regex):
     site = 'http://www.nber.org/data/cps_basic.html'
     parsed = parse(urllib2.urlopen(site))
     root = parsed.getroot()
-    for link in root.iterlinks():
-        fname = matcher(link, regex)
-        if fname:
+
+    partial_matcher = partial(matcher, regex=regex)
+
+    for link in ifilter(partial_matcher, root.iterlinks()):
+        _, _, _fname, _ = link
+        fname = _fname.split('/')[-1]
+        existing = _exists(os.path.join(out_dir, fname))
+        if not existing:
             downloader(fname, out_dir)
             print('Added {}'.format(fname))
 
