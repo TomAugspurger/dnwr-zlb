5572a2de03e281e7b48714f74d1d067ee6dfdf66
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-10-20 10:44:47 -0500

    ENH: implemented checker for panel

--- data_wrangling/cps_wrangling/panel_construction/checker.py
+++ data_wrangling/cps_wrangling/panel_construction/checker.py
@@ -10,50 +10,66 @@ class Checker(object):
 
     def __init__(self, settings, kind):
 
-        if isinstance(str, settings):
+        if isinstance(settings, str):
             with open(settings, 'r') as f:
                 settings = json.load(f)
 
         self.settings = settings
-        self.canon = self.get_canon()
-        self.list_to_check = self.get_list_to_check()
         self.kind = kind
-        # self.cps_store = pd.HDFStore(settings["store_path"])
-        self.get_cps_store = pd.get_store(settings["store_path"])
-        self.get_full_store = pd.get_store(settings["panel_path"])
-        self.get_earn_store = pd.get_store(settings["earn_store_path"])
+        #----------------------------------------------------------------------
+        # Load stores
+        self.cps_store = pd.HDFStore(settings["store_path"])
+        self.panel = pd.HDFStore(settings["panel_path"])
+        #-----------------------------------------------------------------------
+        # Todo List with reference
+        self.canon = self.get_canon()
+        self.all_keys = filter(lambda x: x.startswith('m'),
+                               dir(self.cps_store.root.monthly.data))
+        self.panel_keys = self.panel.keys()
 
     def get_canon(self):
         """
         Treat August 2013's columns as the One True Representation.
         """
-
-    def get_list_to_check(self):
-        pass
+        return self.cps_store.select('/monthly/data/m2013_08').columns
 
     def __call__(self):
-        return {'full': self._call_full, 'earn': self._call_earn}[self.kind]
+        return {'panel': self._call_panel, 'earn': self._call_earn}[self.kind]()
 
-    def _call_full(self):
-        pass
+    def _call_panel(self):
+        """
+        Setter for self.outlist, a list of dicts of lists
+        """
+        gen = self.get_panel_overlap()
+        self.out_list = list(gen)
+
+    def write_panel(self):
+        """ IO """
+        with open("panel_check.json", 'w') as f:
+            json.dump(self.out_list, f)
+
+    def get_panel_overlap(self):
+        for k in self.panel_keys:
+            cols = self.panel.select(k).minor_axis
+            missing = (self.canon - cols).tolist()
+            extra = (cols - self.canon).tolist()
+            common = self.canon.intersection(cols).tolist()
+            yield {"missing": missing, "extra": extra, "common": common}
+            print("Yiedled {}".format(k))
 
     def _call_earn(self):
-        pass
-
-
-def check_columns():
-    pass
+        raise NotImplementedError
 
 
 def main():
     with open('settings.txt', 'r') as f:
         settings = json.load(f)
 
-    full_check = Checker(settings, kind='full')
+    full_check = Checker(settings, kind='panel')
     full_check()
-
-    earn_check = Checker(settings, kind='earn')
-    earn_check()
+    full_check.write()
+    # earn_check = Checker(settings, kind='earn')
+    # earn_check()
 
 if __name__ == '__main__':
     main()
