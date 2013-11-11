6770e64bcfaf07d76dd0b6aacd627d7572ab63db
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-11-11 16:25:15 -0600

    include script for bars. need to remove file dependency

---
+++ data_wrangling/cps_wrangling/analysis/add_rec_bars.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from lxml import html
+
+def add_rec_bars(ax, df=None, dates=None, **kwargs):
+    p = html.parse('http://research.stlouisfed.org/fred2/help-faq/#graph_recessions')
+    r = p.getroot()
+    d = r.body.get_element_by_id('content').get_element_by_id(
+        'content-container').get_element_by_id('content-2columns-main')
+    
+    if dates is None:
+        dates = pd.read_csv('/Users/tom/bin/rec_dates.csv',
+                            parse_dates=['Peak', 'Trough'])
+    y1, y2 = ax.get_ylim()
+    if df:
+        f = df.index.freq
+    else:
+        f = 'MS'
+    # build the ranges:
+    ranges = [pd.date_range(start=x[1]['Peak'], end=x[1]['Trough'],
+              freq=f) for x in dates.iterrows()]
+
+    for span in ranges:
+        ax.fill_between(span, y1=y1, y2=y2, alpha=.25, color='k')
+    return ax
