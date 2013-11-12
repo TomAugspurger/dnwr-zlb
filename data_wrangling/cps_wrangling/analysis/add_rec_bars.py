328568b0dbc8643cbd61df9313188c543e79807e
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-11-12 13:42:32 -0600

    accept kwargs in add_rec_bars

--- data_wrangling/cps_wrangling/analysis/add_rec_bars.py
+++ data_wrangling/cps_wrangling/analysis/add_rec_bars.py
@@ -2,11 +2,11 @@ import pandas as pd
 from lxml import html
 
 def add_rec_bars(ax, df=None, dates=None, **kwargs):
-    p = html.parse('http://research.stlouisfed.org/fred2/help-faq/#graph_recessions')
-    r = p.getroot()
-    d = r.body.get_element_by_id('content').get_element_by_id(
-        'content-container').get_element_by_id('content-2columns-main')
-    
+    # p = html.parse('http://research.stlouisfed.org/fred2/help-faq/#graph_recessions')
+    # r = p.getroot()
+    # d = r.body.get_element_by_id('content').get_element_by_id(
+    #     'content-container').get_element_by_id('content-2columns-main')
+
     if dates is None:
         dates = pd.read_csv('/Users/tom/bin/rec_dates.csv',
                             parse_dates=['Peak', 'Trough'])
@@ -19,6 +19,9 @@ def add_rec_bars(ax, df=None, dates=None, **kwargs):
     ranges = [pd.date_range(start=x[1]['Peak'], end=x[1]['Trough'],
               freq=f) for x in dates.iterrows()]
 
+    alpha = kwargs.pop('alpha', .25)
+    color = kwargs.pop('color', 'k')
+
     for span in ranges:
-        ax.fill_between(span, y1=y1, y2=y2, alpha=.25, color='k')
+        ax.fill_between(span, y1=y1, y2=y2, alpha=alpha, color=color, **kwargs)
     return ax
