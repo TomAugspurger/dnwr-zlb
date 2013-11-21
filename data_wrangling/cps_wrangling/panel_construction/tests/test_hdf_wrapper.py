a13f770453d0662961641fa43c9a81cb57950e4a
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-11-20 20:20:57 -0600

    TST: add some sanitze tests

--- data_wrangling/cps_wrangling/panel_construction/tests/test_hdf_wrapper.py
+++ data_wrangling/cps_wrangling/panel_construction/tests/test_hdf_wrapper.py
@@ -30,15 +30,15 @@ class TestHDFWrapper(unittest.TestCase):
         frequency = 'Q'
         handler = HDFHandler(settings, kind='panel', months=months,
                              frequency=frequency)
-        assert handler.stores.keys() == ['long_1994_Q1']
+        self.assertEqual(handler.stores.keys(), ['long_1994_Q1'])
 
     def test_getitem(self):
         result = self.handler['1994_01']
         expected = self.handler.stores['m1994_01']
-        assert result is expected
+        self.assertIs(result, expected)
 
         result = self.handler['m1994_01']
-        assert result is expected
+        self.assertIs(result, expected)
 
     def test_write(self):
         df = pd.DataFrame({'A': [1, 2, 3]})
@@ -49,18 +49,40 @@ class TestHDFWrapper(unittest.TestCase):
     def test_iter(self):
         result = [x for x in self.handler]
         expected = ['m1994_01', 'm1994_02', 'm1994_03']
-        assert result == expected
+        self.assertEqual(result, expected)
 
     # def test_select_all(self):
     #     import ipdb; ipdb.set_trace()
     #     h = HDFHandler(self.settings, 'panel', frequency='monthly')
     #     assert len(h.stores) == 3
 
+    def test_sanitize_key(self):
+        result = self.handler._sanitize_key('1994-01')
+        expected = 'm1994_01'
+        self.assertEqual(result, expected)
+
+        result = self.handler._sanitize_key('1994_01')
+        self.assertEqual(result, expected)
+
+        result = self.handler._sanitize_key('m1994-01')
+        self.assertEqual(result, expected)
+
+        # With a different pre
+        settings = {'base_path': './test_files/'}
+        months = [['1994_01', '1994_02', '1994_03']]
+        frequency = 'Q'
+        # handler = HDFHandler(settings, kind='long', months=months,
+        #                      frequency=frequency)
+        # result = handler._sanitize_key('long_1996_Q1')
+        # print(handler.pre)
+        # expected = "long_1996_Q1"
+        # self.assertEqual(result, expected)
+
     def tearDown(self):
         self.handler.close()
         def unlink(subdir):
             for f in os.listdir(os.path.join('test_files', subdir)):
-                file_path = os.path.join(self.fdir, f)
+                file_path = os.path.join('.', 'test_files', subdir, f)
                 try:
                     if os.path.isfile(file_path):
                         os.unlink(file_path)
@@ -68,7 +90,9 @@ class TestHDFWrapper(unittest.TestCase):
                     pass
                 except Exception, e:
                     print e
+
             os.rmdir(os.path.join('test_files', subdir))
         unlink('panel')
+        # unlink('long')
         os.rmdir('test_files')
 
