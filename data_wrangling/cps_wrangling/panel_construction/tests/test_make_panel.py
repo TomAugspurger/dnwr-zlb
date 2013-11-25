128058c1e1e5ce3b1a92bca5b700d097c1d28494
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-11-25 13:10:34 -0600

    TST: test match_panel

--- data_wrangling/cps_wrangling/panel_construction/tests/test_make_panel.py
+++ data_wrangling/cps_wrangling/panel_construction/tests/test_make_panel.py
@@ -3,25 +3,29 @@ import unittest
 import pandas as pd
 import pandas.util.testing as tm
 
-from data_wrangling.cps_wrangling.panel_construction.make_panel import huhhnum_to_hrhhid_index
+from data_wrangling.cps_wrangling.panel_construction.make_panel import match_panel
 
 
 class TestMakePanel(unittest.TestCase):
 
-    def test_huhhnum_to_hrhhid(self):
-        # yes, no, no
-        idx1 = pd.MultiIndex.from_tuples([(1, 1, 1), (1, 1, 1), (2, 3, 1)])
-        idx2 = pd.MultiIndex.from_tuples([(1, 11, 1), (1, 12, 1), (1, 13, 1)])
-
-        df1 = pd.DataFrame({'timestamp': pd.to_datetime(3 * ['2003-02-01'])}, index=idx1)
-        df2 = pd.DataFrame({'timestamp': pd.to_datetime(3 * ['2003-02-01'])}, index=idx2)
-
-        df1.index.names = ['HRHHID', 'HRHHID2', 'PULINENO']
-        df2.index.names = ['HRHHID', 'HRHHID2', 'PULINENO']
-        result = huhhnum_to_hrhhid_index(df1, df2)
-        expected = pd.DataFrame({'timestamp': pd.to_datetime(3 * ['2003-02-01'])},
-                                index=pd.MultiIndex.from_tuples([(1, 1, 1),
-                                                                 (1, 2, 1),
-                                                                 (1, 3, 1)]))
-        expected.index.names = ['HRHHID', 'HRHHID2', 'PULINENO']
-        tm.assert_frame_equal(result, expected)
+    def test_match_panel(self):
+        idx1 = pd.MultiIndex.from_tuples([(1, 'one'), (1, 'two'),
+                                          (2, 'one'), (2, 'two'),
+                                          (3, 'one'), (3, 'two'),
+                                          (4, 'one'), (4, 'two'),
+                                          ('six', 'A')])
+        idx2 = pd.MultiIndex.from_tuples([(1, 'one'), (1, 'two'),
+                                          (2, 'one'), (2, 'two'),
+                                          (3, 'one'), (3, 'two'),
+                                          (4, 'one'), (4, 'two'),
+                                          ('ten', 'A')])
+        df1 = pd.DataFrame({'PRTAGE': [4, 4, 4, 4, 4, 4, 4, 4, 10],
+                            'PESEX':  [1, 1, 1, 1, 1, 1, 0, 0, 10]}, index=idx1)
+        df2 = pd.DataFrame({'PRTAGE': [5, 4, 9, 1, 4, 4, 4, 4, 10],
+                            'PESEX':  [1, 1, 1, 1, 1, 0, 1, 0, 10]}, index=idx2)
+        result = match_panel(df1, df2).sort_index()
+        eidx = pd.MultiIndex.from_tuples([(1, 'one'), (1, 'two'),
+                                          (3, 'one'), (4, 'two')])
+        expected = pd.DataFrame({'PRTAGE': [5, 4, 4, 4],
+                                 'PESEX': [1, 1, 1, 0]}, index=eidx)
+        tm.assert_frame_equal(result, expected, check_dtype=False)
